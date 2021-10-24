1aa4da93d78b80262c731237ac6378ff81005dfb
Youssef sellami <youssef-sellami@outlook.com>
2021-10-24 21:55:05 +0100

    added the DI project

---
+++ src/Countries.DependencyInjection/Configurations.cs
@@ -0,0 +1,40 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+    using Countries;
+    using System;
+
+    /// <summary>
+    /// the Configurations class
+    /// </summary>
+    public static class Configurations
+    {
+        /// <summary>
+        /// add the countries configuration.
+        /// </summary>
+        /// <param name="serviceCollection">the service collection instant</param>
+        /// <param name="config">the service collection instant</param>
+        public static IServiceCollection AddCountries(this IServiceCollection serviceCollection, Action<CountriesConfiguration> config)
+        {
+            if (config is null)
+                throw new ArgumentNullException(nameof(config));
+
+            // load an apply the configuration
+            var configuration = new CountriesConfiguration();
+            config(configuration);
+            configuration.Apply();
+
+            // register the countries service
+            return AddCountries(serviceCollection);
+        }
+
+        /// <summary>
+        /// add the countries configuration.
+        /// </summary>
+        /// <param name="serviceCollection">the service collection instant</param>
+        public static IServiceCollection AddCountries(this IServiceCollection serviceCollection)
+        {
+            // register the countries service
+            return serviceCollection.AddSingleton<ICountriesService, CountriesService>();
+        }
+    }
+}
---
+++ src/Countries.DependencyInjection/CountriesConfiguration.cs
@@ -0,0 +1,48 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+using Countries;
+    using System;
+
+    /// <summary>
+    /// the DI registration configuration
+    /// </summary>
+    public class CountriesConfiguration
+    {
+        /// <summary>
+        /// the default language
+        /// </summary>
+        public string DefaultLanguage { get; set; } = "eng";
+
+        /// <summary>
+        /// true to use the current culture info when retrieving the name of the country
+        /// </summary>
+        public bool UseCurrentCultureInfoForCountryName { get; set; } = false;
+
+        /// <summary>
+        /// true to return the Official Name when retrieving the name of the country
+        /// </summary>
+        public bool UserOfficialNameWhenRetrievingCountryName { get; set; } = true;
+
+        /// <summary>
+        /// if true an exception of type <see cref="Domain.Exceptions.TranslationNotFoundExceptionsException"/> will be thrown, if false null will be returned
+        /// </summary>
+        public bool ThrowIfNameNotFoundForALanguage { get; set; } = true;
+
+        /// <summary>
+        /// this option is used when checking equality of the country name, if set to `true` case will be ignored, if `false` the equality will be case sensitive.
+        /// </summary>
+        public bool IgnoreCaseOnNameEqualityCheck { get; set; } = true;
+
+        /// <summary>
+        /// apply the configuration
+        /// </summary>
+        internal void Apply()
+        {
+            WorldCountriesConfiguration.DefaultLanguage = DefaultLanguage;
+            WorldCountriesConfiguration.IgnoreCaseOnNameEqualityCheck = IgnoreCaseOnNameEqualityCheck;
+            WorldCountriesConfiguration.ThrowIfNameNotFoundForALanguage = ThrowIfNameNotFoundForALanguage;
+            WorldCountriesConfiguration.UseCurrentCultureInfoForCountryName = UseCurrentCultureInfoForCountryName;
+            WorldCountriesConfiguration.UserOfficialNameWhenRetrievingCountryName = UserOfficialNameWhenRetrievingCountryName;
+        }
+    }
+}
