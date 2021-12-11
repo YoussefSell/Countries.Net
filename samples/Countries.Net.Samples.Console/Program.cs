66f62d0871afa6c330447d4aac5a65726391dbc2
Youssef sellami <youssef-sellami@outlook.com>
2021-12-11 12:49:37 +0100

    added samples projects code

--- samples/Countries.Net.Samples.Console/Program.cs
+++ samples/Countries.Net.Samples.Console/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var table = new ConsoleTable("Official name", "Common name", "CCA2", "CCA3", "Region");
+var countriesService = new CountriesService();
+
+foreach (var country in countriesService.GetAll())
+{
+    table.AddRow(
+        country.Name.GetName("eng", true),
+        country.Name.GetName("eng", false),
+        country.CCA2,
+        country.CCA3,
+        country.Region
+    );
+}
+
+table.Write(Format.MarkDown);
